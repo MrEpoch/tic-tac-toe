c3262cd78e38af4369f331a4a904e4e887145a5b
MrEpoch <simplymercury@proton.me>
2023-01-07 18:29:11 +0100

    i repaired it little bit, this is commit before adding comments

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
-const placeContent = () => {
-  const placeArr = [];
-  for (let i = 1; i <= 9; i++) {
-    const place = ".spot" + i;
-    const Htmlplace = document.querySelector(place);
-    placeArr.push(Htmlplace);
-  }
-  return placeArr;
-};
-
-const Players = {
-  Player1: `<svg class="circle" viewBox="0 0 24 24">
-    <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"></path>
-  </svg>`,
-  Player2: `<svg class="cross" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"></path>
-                </svg>`,
-};
-
-let arrL = [];
-
-// 0 1 2
-// 3 4 5
-// 6 7 8
 const logic = () => {
+  const round = [];
+  const placeContent = () => {
+    const placementArr = [];
+    for (let i = 1; i <= 9; i += 1) {
+      const place = ".spot" + i;
+      const Htmlplace = document.querySelector(place);
+      placementArr.push(Htmlplace);
+    }
+    return placementArr;
+  };
+  const Players = {
+    Player1: `<svg class="circle" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"></path>
+        </svg>`,
+    Player2: `<svg class="cross" viewBox="0 0 24 24">
+                              <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"></path>
+                      </svg>`,
+  };
   const positionArr = placeContent();
-  const winCheck = () => {
+  const stateCheck = () => {
     const Winner1 =
       (positionArr[0].innerHTML === Players.Player1 &&
         positionArr[1].innerHTML === Players.Player1 &&
@@ -75,36 +69,45 @@ const logic = () => {
       (positionArr[2].innerHTML === Players.Player2 &&
         positionArr[4].innerHTML === Players.Player2 &&
         positionArr[6].innerHTML === Players.Player2);
-    return { Winner1, Winner2 };
+    const tie = round.length === 10;
+    return { Winner1, Winner2, tie };
   };
-  const winEffect = () => {
+  const stateEffect = () => {
     const winP1 = () => {
       document.body.innerHTML = `<div class="win"><p>Player 1 has Won, congratulations!</p><p>X</p></div>`;
     };
     const winP2 = () => {
       document.body.innerHTML = `<div class="win2"><p>Player 2 has Won, congratulations!</p><p>O</p></div>`;
     };
-    return { winP1, winP2 };
+    const tie = () => {
+      document.body.innerHTML = `<div class="tie"><p>Tie!</p></div>`;
+    };
+    return { winP1, winP2, tie };
   };
   const readingArr = () => {
-    for (let round = 0; round < positionArr.length; round++) {
-      positionArr[round].addEventListener("click", () => {
-        arrL.push("empty");
-        if (arrL.length % 2 === 0) {
-          positionArr[round].innerHTML = Players.Player1;
+    for (let i = 0; i < positionArr.length; i += 1) {
+      positionArr[i].addEventListener("click", () => {
+        if (round.length % 2 === 0 && positionArr[i].innerHTML === "") {
+          round.push("empty");
+          positionArr[i].innerHTML = Players.Player1;
+        } else if (round.length % 2 !== 0 && positionArr[i].innerHTML === "") {
+          round.push("empty");
+          positionArr[i].innerHTML = Players.Player2;
         } else {
-          positionArr[round].innerHTML = Players.Player2;
+          round.push("empty");
         }
 
-        if (winCheck().Winner1) {
-          winEffect().winP2();
-        } else if (winCheck().Winner2) {
-          winEffect().winP1();
+        if (stateCheck().Winner1) {
+          stateEffect().winP2();
+        } else if (stateCheck().Winner2) {
+          stateEffect().winP1();
+        }
+        if (stateCheck().tie) {
+          stateEffect().tie();
         }
       });
     }
   };
-  return { readingArr, arrL };
+  return { readingArr, round };
 };
-let caller = logic();
-caller.readingArr();
+logic().readingArr();
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,38 +1,38 @@
 "use strict";
 
-var placeContent = function placeContent() {
-  var placeArr = [];
-
-  for (var i = 1; i <= 9; i++) {
-    var place = ".spot" + i;
-    var Htmlplace = document.querySelector(place);
-    placeArr.push(Htmlplace);
-  }
-
-  return placeArr;
-};
-
-var Players = {
-  Player1: "<svg class=\"circle\" viewBox=\"0 0 24 24\">\n    <path fill=\"currentColor\" d=\"M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z\"></path>\n  </svg>",
-  Player2: "<svg class=\"cross\" viewBox=\"0 0 24 24\">\n                        <path fill=\"currentColor\" d=\"M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\"></path>\n                </svg>"
-};
-var arrL = []; // 0 1 2
-// 3 4 5
-// 6 7 8
-
 var logic = function logic() {
+  var round = [];
+
+  var placeContent = function placeContent() {
+    var placementArr = [];
+
+    for (var i = 1; i <= 9; i += 1) {
+      var place = ".spot" + i;
+      var Htmlplace = document.querySelector(place);
+      placementArr.push(Htmlplace);
+    }
+
+    return placementArr;
+  };
+
+  var Players = {
+    Player1: "<svg class=\"circle\" viewBox=\"0 0 24 24\">\n          <path fill=\"currentColor\" d=\"M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z\"></path>\n        </svg>",
+    Player2: "<svg class=\"cross\" viewBox=\"0 0 24 24\">\n                              <path fill=\"currentColor\" d=\"M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\"></path>\n                      </svg>"
+  };
   var positionArr = placeContent();
 
-  var winCheck = function winCheck() {
+  var stateCheck = function stateCheck() {
     var Winner1 = positionArr[0].innerHTML === Players.Player1 && positionArr[1].innerHTML === Players.Player1 && positionArr[2].innerHTML === Players.Player1 || positionArr[2].innerHTML === Players.Player1 && positionArr[5].innerHTML === Players.Player1 && positionArr[8].innerHTML === Players.Player1 || positionArr[6].innerHTML === Players.Player1 && positionArr[7].innerHTML === Players.Player1 && positionArr[8].innerHTML === Players.Player1 || positionArr[3].innerHTML === Players.Player1 && positionArr[4].innerHTML === Players.Player1 && positionArr[5].innerHTML === Players.Player1 || positionArr[0].innerHTML === Players.Player1 && positionArr[3].innerHTML === Players.Player1 && positionArr[6].innerHTML === Players.Player1 || positionArr[1].innerHTML === Players.Player1 && positionArr[4].innerHTML === Players.Player1 && positionArr[7].innerHTML === Players.Player1 || positionArr[0].innerHTML === Players.Player1 && positionArr[4].innerHTML === Players.Player1 && positionArr[8].innerHTML === Players.Player1 || positionArr[2].innerHTML === Players.Player1 && positionArr[4].innerHTML === Players.Player1 && positionArr[6].innerHTML === Players.Player1;
     var Winner2 = positionArr[0].innerHTML === Players.Player2 && positionArr[1].innerHTML === Players.Player2 && positionArr[2].innerHTML === Players.Player2 || positionArr[2].innerHTML === Players.Player2 && positionArr[5].innerHTML === Players.Player2 && positionArr[8].innerHTML === Players.Player2 || positionArr[6].innerHTML === Players.Player2 && positionArr[7].innerHTML === Players.Player2 && positionArr[8].innerHTML === Players.Player2 || positionArr[3].innerHTML === Players.Player2 && positionArr[4].innerHTML === Players.Player2 && positionArr[5].innerHTML === Players.Player2 || positionArr[0].innerHTML === Players.Player2 && positionArr[3].innerHTML === Players.Player2 && positionArr[6].innerHTML === Players.Player2 || positionArr[1].innerHTML === Players.Player2 && positionArr[4].innerHTML === Players.Player2 && positionArr[7].innerHTML === Players.Player2 || positionArr[0].innerHTML === Players.Player2 && positionArr[4].innerHTML === Players.Player2 && positionArr[8].innerHTML === Players.Player2 || positionArr[2].innerHTML === Players.Player2 && positionArr[4].innerHTML === Players.Player2 && positionArr[6].innerHTML === Players.Player2;
+    var tie = round.length === 10;
     return {
       Winner1: Winner1,
-      Winner2: Winner2
+      Winner2: Winner2,
+      tie: tie
     };
   };
 
-  var winEffect = function winEffect() {
+  var stateEffect = function stateEffect() {
     var winP1 = function winP1() {
       document.body.innerHTML = "<div class=\"win\"><p>Player 1 has Won, congratulations!</p><p>X</p></div>";
     };
@@ -41,41 +41,51 @@ var logic = function logic() {
       document.body.innerHTML = "<div class=\"win2\"><p>Player 2 has Won, congratulations!</p><p>O</p></div>";
     };
 
+    var tie = function tie() {
+      document.body.innerHTML = "<div class=\"tie\"><p>Tie!</p></div>";
+    };
+
     return {
       winP1: winP1,
-      winP2: winP2
+      winP2: winP2,
+      tie: tie
     };
   };
 
   var readingArr = function readingArr() {
-    var _loop = function _loop(round) {
-      positionArr[round].addEventListener("click", function () {
-        arrL.push("empty");
-
-        if (arrL.length % 2 === 0) {
-          positionArr[round].innerHTML = Players.Player1;
+    var _loop = function _loop(i) {
+      positionArr[i].addEventListener("click", function () {
+        if (round.length % 2 === 0 && positionArr[i].innerHTML === "") {
+          round.push("empty");
+          positionArr[i].innerHTML = Players.Player1;
+        } else if (round.length % 2 !== 0 && positionArr[i].innerHTML === "") {
+          round.push("empty");
+          positionArr[i].innerHTML = Players.Player2;
         } else {
-          positionArr[round].innerHTML = Players.Player2;
+          round.push("empty");
         }
 
-        if (winCheck().Winner1) {
-          winEffect().winP2();
-        } else if (winCheck().Winner2) {
-          winEffect().winP1();
+        if (stateCheck().Winner1) {
+          stateEffect().winP2();
+        } else if (stateCheck().Winner2) {
+          stateEffect().winP1();
+        }
+
+        if (stateCheck().tie) {
+          stateEffect().tie();
         }
       });
     };
 
-    for (var round = 0; round < positionArr.length; round++) {
-      _loop(round);
+    for (var i = 0; i < positionArr.length; i += 1) {
+      _loop(i);
     }
   };
 
   return {
     readingArr: readingArr,
-    arrL: arrL
+    round: round
   };
 };
 
-var caller = logic();
-caller.readingArr();
+logic().readingArr();
